e06e0f94c170284e92d912ef5c0da529737eddbc
Shashankkumar-github <101034283+Shashankkumar-github@users.noreply.github.com>
2025-03-20 23:08:11 +0530

    Employee Hour Wages

--- CalculateWage.js
+++ CalculateWage.js
@@ -3,7 +3,7 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-
+// UC2
 // Compute Employee Hours
 let empHrs = 0;
 let empCheck = Math.floor(Math.random() * 10) % 3;
@@ -22,3 +22,21 @@ switch (empCheck) {
 // Calculate Employee Wage
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Employee Wage: " + empWage);
+
+//UC3
+
+function getWorkingHrs(emp){
+    switch(emp){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HOURS;
+                default:
+                    return 0;
+    }
+}
+let Hrs=0;
+let emp=Math.floor(Math.random()*10)%3;
+Hrs=getWorkingHrs(emp);
+empWage=Hrs*WAGE_PER_HOUR;
+console.log("Emp wage:"+empWage);
--- Employee_Presence.js
+++
@@ -1,8 +0,0 @@
-const Isabsent=0
-let emp=Math.floor(Math.random()*10)%2;
-if(emp==Isabsent){
-    console.log("Employee is Absent");
-    return;
-}else{
-    console.log("Employee is Present");
-}
