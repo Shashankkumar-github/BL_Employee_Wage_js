59edd92ecac66602a4224ce23c0352e9261872cd
Shashankkumar-github <101034283+Shashankkumar-github@users.noreply.github.com>
2025-03-21 10:49:18 +0530

    Employee Monthly Wage

--- CalculateWage.js
+++ CalculateWage.js
@@ -3,6 +3,7 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;  
 // UC2
 // Compute Employee Hours
 let empHrs = 0;
@@ -40,3 +41,29 @@ let emp=Math.floor(Math.random()*10)%3;
 Hrs=getWorkingHrs(emp);
 empWage=Hrs*WAGE_PER_HOUR;
 console.log("Emp wage:"+empWage);
+
+// UC4
+// Function to Get Work Hours  
+function getWorkingHours(empCheck) {  
+    switch (empCheck) {  
+        case IS_PART_TIME:  
+            return PART_TIME_HOURS;  
+        case IS_FULL_TIME:  
+            return FULL_TIME_HOURS;  
+        default:  
+            return 0;  
+    }  
+}  
+
+// Compute Total Employee Wage for a Month  
+let totalEmpHrs = 0; 
+
+
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {  
+    let empCheck = Math.floor(Math.random() * 10) % 3;  
+    totalEmpHrs += getWorkingHours(empCheck);  
+}  
+  
+let totalEmpWage = totalEmpHrs * WAGE_PER_HOUR;  
+console.log("Total Employee Wage for " + NUM_OF_WORKING_DAYS + " days: $" + totalEmpWage);
+
